b1fff8afebe971d633e7b47fae7c17c51cd148e5
Petrik Diak <diak@petrik.hu>
2025-05-09 11:24:08 +0200

    Kerdes osztaly kész, talán

---
+++ Milliomos_Jatek/Kerdes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milliomos_Jatek
+{
+	internal class Kerdes
+	{
+		List<string> valasz;
+		string helyes_valasz;
+		string kategoria;
+
+		public Kerdes(List<string> valasz, string helyes_valasz, string kategoria)
+		{
+			this.valasz = valasz;
+			this.helyes_valasz = helyes_valasz;
+			this.kategoria = kategoria;
+		}
+
+		public List<string> Valasz { get => valasz; set => valasz = value; }
+		public string Helyes_valasz { get => helyes_valasz; set => helyes_valasz = value; }
+		public string Kategoria { get => kategoria; set => kategoria = value; }
+
+		//public override string? ToString()
+		//{
+		//	return $"{valasz}, {helyes_valasz}, {kategoria}";
+		//}
+	}
+}
