94b5dc0a2bc115f34dc06dae42595e64afbe6be1
Petrik Diak <diak@petrik.hu>
2025-05-09 12:13:07 +0200

    Sorkerdes osztaly kész, kerdes osztaly javítva

--- Milliomos_Jatek/Kerdes.cs
+++ Milliomos_Jatek/Kerdes.cs
@@ -8,24 +8,32 @@ namespace Milliomos_Jatek
 {
 	internal class Kerdes
 	{
-		List<string> valasz;
+		int kodszam;
+		string question;
+		string[] valasz;
 		string helyes_valasz;
 		string kategoria;
 
-		public Kerdes(List<string> valasz, string helyes_valasz, string kategoria)
+		public Kerdes(int kodszam, string question, string[] valasz, string helyes_valasz, string kategoria)
 		{
+			this.kodszam = kodszam;
+			this.question = question;
 			this.valasz = valasz;
 			this.helyes_valasz = helyes_valasz;
 			this.kategoria = kategoria;
 		}
 
-		public List<string> Valasz { get => valasz; set => valasz = value; }
+		public int Kodszam { get => kodszam; set => kodszam = value; }
+		public string Question { get => question; set => question = value; }
+		public string[] Valasz { get => valasz; set => valasz = value; }
 		public string Helyes_valasz { get => helyes_valasz; set => helyes_valasz = value; }
 		public string Kategoria { get => kategoria; set => kategoria = value; }
 
+
+
 		//public override string? ToString()
 		//{
-		//	return $"{valasz}, {helyes_valasz}, {kategoria}";
+		//	return $"{kodszam}, {question}, {valasz}, {helyes_valasz}, {kategoria}";
 		//}
 	}
 }
---
+++ Milliomos_Jatek/sorkerdes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milliomos_Jatek
+{
+	internal class Sorkerdes
+	{
+		string sorQuestion;
+		string[] sorValasz;
+		string helyesSorValasz;
+		string sorKategoria;
+
+		public Sorkerdes(string sorQuestion, string[] sorValasz, string helyesSorValasz, string sorKategoria)
+		{
+			this.sorQuestion = sorQuestion;
+			this.sorValasz = sorValasz;
+			this.helyesSorValasz = helyesSorValasz;
+			this.sorKategoria = sorKategoria;
+		}
+
+		public string SorQuestion { get => sorQuestion; set => sorQuestion = value; }
+		public string[] SorValasz { get => sorValasz; set => sorValasz = value; }
+		public string HelyesSorValasz { get => helyesSorValasz; set => helyesSorValasz = value; }
+		public string SorKategoria { get => sorKategoria; set => sorKategoria = value; }
+
+		//public override string? ToString()
+		//{
+		//	return $"{sorQuestion}, {sorValasz}, {helyesSorValasz}, {sorKategoria}";
+		//}
+	}
+}
